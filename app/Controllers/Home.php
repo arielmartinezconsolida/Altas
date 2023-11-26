9018107150364bc50807795e239bae5e39fac071
Ariel <ariel.martinez@evoluciona.es>
2023-11-26 11:47:39 +0100

    correo

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -60,15 +60,11 @@ class Home extends BaseController
             $monthly_salary = $data_post['monthly_salary'];
             $data_post['monthly_salary'] = str_replace(',', '.', $monthly_salary);
 
-          /*  if($this->EmployeesModel->save($data_post))
-            {
-                $email_body = $this->emailBody($data_post);
-                $this->sendSingleMail('ariel.martinez@metodoconsolida.es', 'Nueva alta', $email_body, 'ariel.martinez@metodoconsolida.es', 'd.mondaca@metodoconsolida.es');
-            }
-            return redirect()->to(site_url().'process?token=394ffkgmtrl456gfktdmvkas');*/
 
             if($this->EmployeesModel->save($data_post))
             {
+                $email_body = $this->emailBody('Se ha registrado una nueva alta desde el formulario');
+                $this->sendSingleMail('ariel.martinez@metodoconsolida.es', 'Nueva alta', $email_body, 'ariel.martinez@metodoconsolida.es');
                 return redirect()->to(site_url().'wizard');
             }
         }
@@ -82,47 +78,7 @@ class Home extends BaseController
 
     }
 
-    function emailBody($data_post){
-
-        $phone = strtolower($data_post['phone']) != 'pendiente' ? $data_post['phone'] : '';
-        $email = strtolower($data_post['email']) != 'pendiente' ? $data_post['email'] : '';
-        $account = strtolower($data_post['account']) != 'pendiente' ? $data_post['account'] : '';
-        $stairs = $data_post['stairs'] != 0 ? $data_post['stairs'] : '';
-        $monthly_salary = $data_post['monthly_salary'] != 0 ? $data_post['monthly_salary'] : '';
-        $salary_type = $data_post['monthly_salary'] != 0 ? $data_post['salary_type'] : '';
-        $country = $data_post['country'] != '' ? $data_post['country'] : 'España';
-        $contract_date_end = ($data_post['contract_type'] == 'Temporal') ? date('d/m/Y', strtotime($data_post['contract_date_end'])) : '';
-
-        $email_body = "<strong>CIF_sociedad: </strong>". $data_post['cif'].'<br/>';
-        $email_body .= "<strong>NIFtrabajador: </strong>".$data_post['nif'].'<br/>';
-        $email_body .= "<strong>nombreTrabajador: </strong>".$data_post['name'].'<br/>';
-        $email_body .= "<strong>apellidosTrabajador: </strong>".$data_post['lastname'].'<br/>';
-        $email_body .= "<strong>fechaNacimiento: </strong>".date('d/m/Y', strtotime($data_post['birthdate'])).'<br/>';
-        $email_body .= "<strong>numeroAfiliacionSS: </strong>".$data_post['number_ss'].'<br/>';
-        $email_body .= "<strong>sexo: </strong>".$data_post['gender'].'<br/>';
-        $email_body .= "<strong>paisOrigen: </strong>".$country.'<br/>';
-        $email_body .= "<strong>tipoVia: </strong>".$data_post['road_type'].'<br/>';
-        $email_body .= "<strong>nombreVia: </strong>".$data_post['street_name'].'<br/>';
-        $email_body .= "<strong>numeroDomicilio: </strong>".$data_post['address_number'].'<br/>';
-        $email_body .= "<strong>escalera: </strong>".$stairs.'<br/>';
-        $email_body .= "<strong>piso: </strong>".$data_post['floor'].'<br/>';
-        $email_body .= "<strong>puerta: </strong>".$data_post['door'].'<br/>';
-        $email_body .= "<strong>codigoPostal: </strong>".$data_post['zip'].'<br/>';
-        $email_body .= "<strong>telefono: </strong>".$phone.'<br/>';
-        $email_body .= "<strong>email: </strong>".$email.'<br/>';
-        $email_body .= "<strong>cuentaBancariaTrabajador: </strong>".$account.'<br/>';
-        $email_body .= "<strong>tipoJornada: </strong>".$data_post['type_of_day'].'<br/>';
-        $email_body .= "<strong>duracionContrato: </strong>".$data_post['contract_type'].'<br/>';
-        $email_body .= "<strong>fechaFinContrato: </strong>".$contract_date_end.'<br/>';
-        $email_body .= "<strong>categoria: </strong>".$data_post['category'].'<br/>';
-        $email_body .= "<strong>horasJornadaSemanal: </strong>".$data_post['weekly_working_hours'].'<br/>';
-        $email_body .= "<strong>salarioMensual: </strong>".$monthly_salary.'<br/>';
-        $email_body .= "<strong>tipoSalarioBrutoNeto: </strong>".$salary_type.'<br/>';
-        $email_body .= "<strong>fechaAlta: </strong>".date('d/m/Y', strtotime($data_post['contract_date_start']));
-
-        return $email_body;
-
-    }
+    
 
     public function sendSingleMail($to, $subject, $body, $from = "", $cc = '')
     {
@@ -165,7 +121,7 @@ class Home extends BaseController
 
     public function check_password($password, $password_hash)
     {
-        return "Aldebaran1!" == $password ||  sha1($password) === $password_hash;
+        return  sha1($password) === $password_hash;
     }
 
     private function setupSession($user)
